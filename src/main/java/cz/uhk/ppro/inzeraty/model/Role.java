6c465e7356924dcf621e35a784fc5334de5b0d4d
Pavel Borik <pavel.borik1@gmail.com>
2017-11-13 18:43:41 +0100

    added role

---
+++ src/main/java/cz/uhk/ppro/inzeraty/model/Role.java
@@ -0,0 +1,48 @@
+package cz.uhk.ppro.inzeraty.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "Roles", schema = "inzeraty", catalog = "")
+public class Role {
+    @Id
+    @GeneratedValue
+    private int id;
+    private String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Role role = (Role) o;
+
+        if (id != role.id) return false;
+        if (name != null ? !name.equals(role.name) : role.name != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id;
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        return result;
+    }
+}
