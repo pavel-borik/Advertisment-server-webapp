6f943c3bd736c251ba465bc6295a043d2008f3ed
Pavel Borik <pavel.borik1@gmail.com>
2017-11-15 21:06:07 +0100

    minor changes

--- src/main/java/cz/uhk/ppro/inzeraty/controller/AdvertController.java
+++ src/main/java/cz/uhk/ppro/inzeraty/controller/AdvertController.java
@@ -2,22 +2,32 @@ package cz.uhk.ppro.inzeraty.controller;
 
 import cz.uhk.ppro.inzeraty.model.Advert;
 import cz.uhk.ppro.inzeraty.model.User;
+import cz.uhk.ppro.inzeraty.repository.AdvertRepository;
+import cz.uhk.ppro.inzeraty.repository.jpa.JpaAdvertRepositoryImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import java.sql.Timestamp;
+
 @Controller
 public class AdvertController {
+
+    @Autowired
+    AdvertRepository advertRepo = new JpaAdvertRepositoryImpl();
 
     @RequestMapping(value = "/advert", method = RequestMethod.POST)
     public String create(@ModelAttribute("advert") Advert advert) {
         System.out.println(advert.getName() + " "+ advert.getDescription());
+        advert.setTimestamp(new Timestamp(System.currentTimeMillis()));
+        advertRepo.save(advert);
         return "redirect:advertSuccess";
     }
 
     @RequestMapping(value = "/advert", method = RequestMethod.GET)
-    public String showAdvert(@ModelAttribute("advert") Advert advert) {
+    public String showAdvertForm(@ModelAttribute("advert") Advert advert) {
         return "advert";
     }
 
--- src/main/java/cz/uhk/ppro/inzeraty/controller/RegistrationController.java
+++ src/main/java/cz/uhk/ppro/inzeraty/controller/RegistrationController.java
@@ -1,22 +1,38 @@
 package cz.uhk.ppro.inzeraty.controller;
 
+import cz.uhk.ppro.inzeraty.model.Role;
 import cz.uhk.ppro.inzeraty.model.User;
+import cz.uhk.ppro.inzeraty.repository.UserRepository;
+import cz.uhk.ppro.inzeraty.repository.jpa.JpaUserRepositoryImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import java.sql.Date;
+import java.sql.Timestamp;
+
+
 @Controller
 public class RegistrationController {
+
+    @Autowired
+    UserRepository userRepo = new JpaUserRepositoryImpl();
+
 
     @RequestMapping(value = "/registration", method = RequestMethod.POST)
     public String register(@ModelAttribute("user") User user) {
         System.out.println(user.getEmail() + " "+ user.getPassword());
+        user.setCreationTime(new Timestamp(System.currentTimeMillis()));
+        System.out.println(user.getCreationTime());
+        user.setRole(new Role());
+        userRepo.save(user);
         return "redirect:registrationSuccess";
     }
 
     @RequestMapping(value = "/registration", method = RequestMethod.GET)
-    public String showRegistration(@ModelAttribute("user") User user) {
+    public String showRegistrationForm(@ModelAttribute("user") User user) {
         return "registration";
     }
 
