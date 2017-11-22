4f0f879cb299862b22949a930629ce52e4ec1874
Pavel Borik <pavel.borik1@gmail.com>
2017-11-22 22:48:09 +0100

    username query

--- src/main/java/cz/uhk/ppro/inzeraty/repository/UserRepository.java
+++ src/main/java/cz/uhk/ppro/inzeraty/repository/UserRepository.java
@@ -3,9 +3,11 @@ package cz.uhk.ppro.inzeraty.repository;
 import cz.uhk.ppro.inzeraty.model.User;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface UserRepository {
     List<User> findAll();
     User findById(int id);
+    Optional<User> findByUsername(String username);
     void save(User user);
 }
--- src/main/java/cz/uhk/ppro/inzeraty/repository/jpa/JpaUserRepositoryImpl.java
+++ src/main/java/cz/uhk/ppro/inzeraty/repository/jpa/JpaUserRepositoryImpl.java
@@ -9,6 +9,7 @@ import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
 import javax.transaction.Transactional;
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public class JpaUserRepositoryImpl implements UserRepository {
@@ -17,16 +18,22 @@ public class JpaUserRepositoryImpl implements UserRepository {
     private EntityManager em;
 
     @Override
-    @SuppressWarnings("unchecked")
     public List<User> findAll() {
-        return this.em.createQuery("SELECT usrs FROM Users usrs").getResultList();
+        return this.em.createQuery("SELECT usrs FROM User usrs").getResultList();
     }
 
     @Override
     public User findById(int id) {
-        Query query = this.em.createQuery("SELECT user FROM Users user WHERE user.id =:id");
+        Query query = this.em.createQuery("SELECT u FROM User u WHERE u.id =:id");
         query.setParameter("id", id);
         return (User) query.getSingleResult();
+    }
+
+    @Override
+    public Optional<User> findByUsername(String username) {
+        Query query = this.em.createQuery("SELECT u FROM User u WHERE u.username =:username");
+        query.setParameter("username", username);
+        return (Optional<User>) query.setMaxResults(1).getResultList().stream().findFirst();
     }
 
     @Transactional
